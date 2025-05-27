6b4bf14d453e35c6877c8821238dc3135b1347a2
Abdullah Badres <abdullahbadres@gmail.com>
2025-05-27 15:50:20 +0700

    Final

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -81,7 +81,7 @@ const SocialLink = memo(({ icon: Icon, link }) => (
 const TYPING_SPEED = 100
 const ERASING_SPEED = 50
 const PAUSE_DURATION = 2000
-const WORDS = ["Frontend Developper", "Tech Enthusiast"]
+const WORDS = ["Frontend Developer", "Tech Enthusiast"]
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"]
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/Abdullahbadres" },
